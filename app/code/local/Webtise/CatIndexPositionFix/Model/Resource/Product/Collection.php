e11189361f551a3e2d4bd4f0b2d467428a34ce0a
Unknown <joshcarter94@icloud.com>
2017-02-28 17:07:31 +0000

    Add Join fixes to model override

--- app/code/local/Webtise/CatIndexPositionFix/Model/Resource/Product/Collection.php
+++ app/code/local/Webtise/CatIndexPositionFix/Model/Resource/Product/Collection.php
@@ -71,4 +71,117 @@ class Webtise_CatIndexPositionFix_Model_Resource_Product_Collection extends Mage
 
         return parent::addAttributeToSort($attribute, $dir);
     }
+
+    //TODO - Check Join methods for cat_index_position reference where it should be cat_index.position
+
+    /**
+     * Apply front-end price limitation filters to the collection
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    public function applyFrontendPriceLimitations()
+    {
+        $this->_productLimitationFilters['use_price_index'] = true;
+        if (!isset($this->_productLimitationFilters['customer_group_id'])) {
+            $customerGroupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+            $this->_productLimitationFilters['customer_group_id'] = $customerGroupId;
+        }
+        if (!isset($this->_productLimitationFilters['website_id'])) {
+            $websiteId = Mage::app()->getStore($this->getStoreId())->getWebsiteId();
+            $this->_productLimitationFilters['website_id'] = $websiteId;
+        }
+        $this->_applyProductLimitations();
+        return $this;
+    }
+    /**
+     * Apply limitation filters to collection
+     * Method allows using one time category product index table (or product website table)
+     * for different combinations of store_id/category_id/visibility filter states
+     * Method supports multiple changes in one collection object for this parameters
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    protected function _applyProductLimitations()
+    {
+        Mage::dispatchEvent('catalog_product_collection_apply_limitations_before', array(
+            'collection'  => $this,
+            'category_id' => isset($this->_productLimitationFilters['category_id'])
+                ? $this->_productLimitationFilters['category_id']
+                : null,
+        ));
+        $this->_prepareProductLimitationFilters();
+        $this->_productLimitationJoinWebsite();
+        $this->_productLimitationJoinPrice();
+        $filters = $this->_productLimitationFilters;
+        if (!isset($filters['category_id']) && !isset($filters['visibility'])) {
+            return $this;
+        }
+        $conditions = array(
+            'cat_index.product_id=e.entity_id',
+            $this->getConnection()->quoteInto('cat_index.store_id=?', $filters['store_id'])
+        );
+        if (isset($filters['visibility']) && !isset($filters['store_table'])) {
+            $conditions[] = $this->getConnection()
+                ->quoteInto('cat_index.visibility IN(?)', $filters['visibility']);
+        }
+        if (!$this->getFlag('disable_root_category_filter')) {
+            $conditions[] = $this->getConnection()->quoteInto('cat_index.category_id = ?', $filters['category_id']);
+        }
+        if (isset($filters['category_is_anchor'])) {
+            $conditions[] = $this->getConnection()
+                ->quoteInto('cat_index.is_parent=?', $filters['category_is_anchor']);
+        }
+        $joinCond = join(' AND ', $conditions);
+        $fromPart = $this->getSelect()->getPart(Zend_Db_Select::FROM);
+        if (isset($fromPart['cat_index'])) {
+            $fromPart['cat_index']['joinCondition'] = $joinCond;
+            $this->getSelect()->setPart(Zend_Db_Select::FROM, $fromPart);
+        }
+        else {
+            $this->getSelect()->join(
+                array('cat_index' => $this->getTable('catalog/category_product_index')),
+                $joinCond,
+                array('cat_index.position' => 'position')
+            );
+        }
+        $this->_productLimitationJoinStore();
+        Mage::dispatchEvent('catalog_product_collection_apply_limitations_after', array(
+            'collection' => $this
+        ));
+        return $this;
+    }
+
+    /**
+     * Apply limitation filters to collection base on API
+     * Method allows using one time category product table
+     * for combinations of category_id filter states
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    protected function _applyZeroStoreProductLimitations()
+    {
+        $filters = $this->_productLimitationFilters;
+        $conditions = array(
+            'cat_pro.product_id=e.entity_id',
+            $this->getConnection()->quoteInto('cat_pro.category_id=?', $filters['category_id'])
+        );
+        $joinCond = join(' AND ', $conditions);
+        $fromPart = $this->getSelect()->getPart(Zend_Db_Select::FROM);
+        if (isset($fromPart['cat_pro'])) {
+            $fromPart['cat_pro']['joinCondition'] = $joinCond;
+            $this->getSelect()->setPart(Zend_Db_Select::FROM, $fromPart);
+        }
+        else {
+            $this->getSelect()->join(
+                array('cat_pro' => $this->getTable('catalog/category_product')),
+                $joinCond,
+                array('cat_index.position' => 'position')
+            );
+        }
+        $this->_joinFields['position'] = array(
+            'table' => 'cat_pro',
+            'field' => 'position',
+        );
+        return $this;
+    }
 }
